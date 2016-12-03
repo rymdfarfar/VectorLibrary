a82f682bebcd9de1428c7d03436dd0030e0ea22b
Tintin oterdahl <Tintin oterdahl>
2016-12-03 18:24:00 +1000

    firsst build.

---
+++ VectorLibary/VectorLibary/Test.cpp
@@ -0,0 +1,32 @@
+#include "Test.h"
+#include "Vector2.h"
+#include "Vector3.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+
+Test::Test()
+{
+}
+
+
+Test::~Test()
+{
+}
+
+void Test::Testvector() {
+	kina = vector2i(10, 10);
+	vector3f kina3(10, 10, 10);
+	cout << kina.x;
+	cout << kina.y;
+}
+
+int Test:: main(int argc, char **argv)
+{
+	Testvector();
+	
+	return 0;
+}
---
+++ VectorLibary/VectorLibary/Test.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "vector2.h"
+class Test
+{
+public:
+
+	vector2i kina;
+	
+	Test();
+	~Test();
+	void Testvector();
+
+	int Test::main(int argc, char **argv);
+	
+
+};
+
